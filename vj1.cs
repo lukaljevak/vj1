64a6f262499af75d40cd9f337f2c4dba4b6d1b92
Luka Ljevak <150526921+lukaljevak@users.noreply.github.com>
2023-12-12 12:28:55 +0100

    Add files via upload

---
+++ vj1.cs
@@ -0,0 +1,15 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		    int a = 100;
+            int b = 3;
+
+            int rezultat = a / b;
+            int ostatak = a % b;
+
+            Console.WriteLine("a dijeljeno s b je {0} i ostatak {1}.", rezultat, ostatak);
+	}
+}
